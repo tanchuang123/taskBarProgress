aa498e8e1b7f4f4f05ac906be72f03064b49f9b3
tanchuang <tanchuang@seer-robotics.com>
2018-11-29 14:53:01 +0800

    任务栏进度条

---
+++ widget.cpp
@@ -0,0 +1,49 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+#include <QtWidgets>
+#include <QtWinExtras>
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+   menu = new QMenu(this);
+  if (QtWin::isCompositionEnabled()) {
+      QtWin::enableBlurBehindWindow(menu);
+      QString css("QMenu { border: 1px solid %1; border-radius: 2px; background: transparent; }");
+      menu->setStyleSheet(css.arg(QtWin::realColorizationColor().name()));
+  } else {
+      QtWin::disableBlurBehindWindow(menu);
+      QString css("QMenu { border: 1px solid black; background: %1; }");
+      menu->setStyleSheet(css.arg(QtWin::realColorizationColor().name()));
+  }
+   createTaskbar();
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::seekForward()//滑块快进
+{
+    ui->horizontalSlider->triggerAction(QSlider::SliderPageStepAdd);
+}
+
+void Widget::seekBackward()//滑块后退
+{
+   ui->horizontalSlider->triggerAction(QSlider::SliderPageStepSub);
+}
+
+void Widget::createTaskbar()
+{
+    taskbarButton = new QWinTaskbarButton(this);
+    taskbarButton->setWindow(windowHandle());
+
+    taskbarProgress = taskbarButton->progress();
+    taskbarProgress->show();
+    connect(ui->horizontalSlider, &QAbstractSlider::valueChanged, taskbarProgress, &QWinTaskbarProgress::setValue);
+    connect(ui->horizontalSlider, &QAbstractSlider::rangeChanged, taskbarProgress, &QWinTaskbarProgress::setRange);
+
+}
---
+++ widget.h
@@ -0,0 +1,37 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QMenu>
+#include <QtWinExtras/QWinTaskbarButton>
+#include <QtWinExtras/QWinTaskbarProgress>
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+public slots:
+
+    void seekForward();
+    void seekBackward();
+private:
+
+    void createTaskbar();
+
+
+    QWinTaskbarButton *taskbarButton = nullptr;
+    QWinTaskbarProgress *taskbarProgress = nullptr;
+
+    QMenu *menu;
+
+private:
+    Ui::Widget *ui;
+};
+
+#endif // WIDGET_H
